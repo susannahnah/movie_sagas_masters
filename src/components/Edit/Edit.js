6feb8e2ceddcfb8057fe41335210300ba75c3d30
Susannah Harris <harris.susannah@gmail.com>
2019-06-23 16:41:19 -0500

    created functions for editing title and descriptions

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -6,10 +6,32 @@ class Edit extends Component {
     state = {
         title: '',
         description: '',
-        
+
     }
-    
-}
+
+    handleTitleChange = (event) => {
+        console.log('title changed')
+        this.setState({
+            ...this.state,
+            title: event.target.value
+        })
+    };
+
+
+    handleDescriptionChange = (event) => {
+        console.log('description changed')
+        this.setState({
+            ...this.state,
+        description: event.target.value
+        })
+    };
+
+
+    render()
+};
+
+
+
 
 
 const mapStateToProps = reduxState => ({
